f1cc833b787cbacb32bd84eb3a7b45eb113c46b3
Your Name <your.email@example.com>
2025-04-09 02:06:39 +0530

    Rename Codepen.js to RunWeb.js and update its content

--- src/RunWeb.js
+++ src/RunWeb.js
@@ -2,8 +2,7 @@ import React from 'react';
 import App from './Components/App';
 import {Routes, Route} from 'react-router-dom';
 
-
-export default function Codepen() {
+export default function RunWeb() {
   return (
     <Routes>
       <Route path='/' element={<App />}/>
