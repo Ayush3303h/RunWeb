ab3afe1215a388c79a9ff99394513b706e493d70
Your Name <your.email@example.com>
2025-04-09 02:05:01 +0530

    Update src/index.js with new React code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Codepen from './Codepen.js';
+import Codepen from './RunWeb.js';
 import { BrowserRouter } from 'react-router-dom';
+import RunWeb from './RunWeb.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Codepen />
+    <RunWeb />
   </BrowserRouter>
 );
